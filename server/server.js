e3f346f7ba6f992dfc139cd567dd8d15b6443a44
jahjahblinks <82290362+jahjahblinks@users.noreply.github.com>
2022-05-19 11:42:06 -0700

    clean up dead code

--- server/server.js
+++ server/server.js
@@ -266,9 +266,6 @@ io.on("connection", socket => {
           }
           else {
             socket.to(room.id).emit('start_speech');
-            //CHAT.sendCallback(other, {
-            //  self: `Speech recognition activated!`
-            //});
           }
           break;
 
