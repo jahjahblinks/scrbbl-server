4104a15969a89ddd917099bcc578a4c65caa0257
lsssmmns <97260713+lsssmmns@users.noreply.github.com>
2022-05-26 16:06:08 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -203,7 +203,7 @@ io.on("connection", socket => {
   socket.on("clear_tutorial", () => {
     other = socket;
     clients.forEach(function (cl) {
-      if (socket.name==(cl.name+"7")){
+      if (socket.name==(cl.name+"9")){
         other = cl;
         }
     });
